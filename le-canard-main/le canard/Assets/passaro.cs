06d37c48890484742a523539887371150dfafacc
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-08 09:33:22 -0300

    lorem

---
+++ le-canard-main/le canard/Assets/passaro.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    Rigidbody2D fisica;
+
+    private void Awake()
+    {
+        this.fisica = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+            if (Input.GetMouseButtonDown(0)) 
+            {
+                {
+                this.Impulsionar();
+                }
+            }
+        }
+    void Impulsionar()
+    {
+        this.fisica.AddForce(Vector2.up * 2, ForceMode2D.Impulse);
+       // this.fisica.AddForce(Vector2.right * 2, ForceMode2D.Impulse);
+    }
+}
